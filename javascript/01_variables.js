b411111922e9e1e60f3aaa53930fabfbc98edbdc
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2022-12-25 17:18:38 +0000

    variables printing in a tabular form

---
+++ javascript/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 3370;
+let emailId = "ravi@gmail.com";
+let username = "ravi";
+city = "delhi";
+let state;
+
+console.log("emailId"); 
+
+console.table([accountId, emailId, username , city, state]);
