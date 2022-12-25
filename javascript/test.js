c69e053103d51daf2bf3638a0306bf2ce576278c
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2022-12-25 17:32:45 +0530

    Update test.js

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,13 @@
 console.log("Ravi Singh Chauhan")
+
+// Ternery Operator
+var authenticated = true;
+
+// if(authenticated){
+// console.log("Show Signout Button");
+// }
+// else{
+// console.log("Show Login Button");
+// }
+
+authenticated ? console.log("Show SignOut Button") : console.log("Show SignIn Button");
