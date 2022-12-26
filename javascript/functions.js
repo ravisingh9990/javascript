dac68fda8387cdae45aebbacb398f34581bd6c1a
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2022-12-26 18:26:20 +0000

    type coercion and  basics of functions

---
+++ javascript/functions.js
@@ -0,0 +1,9 @@
+function sayhello(name){
+    console.log("Hello there, ravi");
+    console.log("Hello there,".name);
+
+    // console.log(`Hello there, ${name}. how are you`);
+}
+
+sayhello("Ravi");
+sayhello("lakshay");
---
+++ javascript/type_coercion.js
@@ -0,0 +1,18 @@
+//falsy values
+
+// undefind
+// null
+// ""
+// NaN
+
+var user = "2";
+
+if(2 == user){
+    console.log("condition is true");
+
+}
+
+if(2 === user){
+    console.log("condition is true");
+
+}
