93cb3317f9c0ac46479d5f97d0669b3eedab7673
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2022-12-26 18:31:24 +0000

    new backtick way using variables

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,8 +1,8 @@
 function sayhello(name){
     console.log("Hello there, ravi");
-    console.log("Hello there,".name);
+    //console.log("Hello there,".name);
 
-    // console.log(`Hello there, ${name}. how are you`);
+  console.log(`Hello there, ${name}. how are you`);
 }
 
 sayhello("Ravi");
