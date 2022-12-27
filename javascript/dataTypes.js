9b068680de44f917a980b68d5276d449910ed10c
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2022-12-27 17:02:23 +0000

    dataTypes

---
+++ javascript/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all the js code as newer version
+// alert("Hello"); //we are using node js, not browser
+
+let userName = "Ravi";
+let age = 20;
+let isLogedIn = true;
+let state;
+// primitive data types
+
+// number => 2 to power 53
+// bigint => too big number usually used in stock market or trading
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but not assigned any value to it
+// symbol => to find uniqueness of the component usually used in react, when we make so many components
+
+// object 
+
+console.log(typeof "Ravi"); // string
+console.log(typeof age); // number
+console.log(typeof isLogedIn); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefiend
