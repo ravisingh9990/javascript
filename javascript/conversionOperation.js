d8d11fa1145f71b785da55fd516ee425f0b3836a
ravisingh9990 <89929744+ravisingh9990@users.noreply.github.com>
2022-12-30 18:26:47 +0000

    conversion of data types

---
+++ javascript/conversionOperation.js
@@ -0,0 +1,36 @@
+let score = 33; //ouput is number
+// let score = true; //ouput is 1
+// let score = "33"; //ouput is string
+// let score = 332bc; //ouput is NAN
+// let score = null;  //ouput is 0
+// let score = undefined;  //ouput is NAN
+
+// without parentheses 
+console.log(typeof score);
+// with parentheses 
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(isLoggedIn);  //output is true
+
+// let isLoggedIn = "";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(isLoggedIn);  //output is false
+
+// let isLoggedIn = "Ravi";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(isLoggedIn);  //output is true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
